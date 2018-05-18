5b71b441957bb760e9971fd1ea3bc60ab1e5c47e
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-05-18 11:30:14 -0500

    Reducer returning initialState

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  guesses: [],
+  feedback: 'Make your guess!',
+  auralStatus: '',
+  correctAnswer: Math.round(Math.random() * 100) + 1
+};
+
+export const gameReducer = (state=initialState, action) => {
+  return state;
+};
